7054dce8226d27cb17c3bb526ecfb8e5fc7f4d7f
Junliang HU <jlhu@cse.cuhk.edu.hk>
2022-12-18 23:27:11 +0800

    create csci5120-run.sh from csci5120-test.sh

---
+++ contrib/local-test/csci5120-run.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -ex
+
+DIR="$(dirname "$(readlink -f "$0")")"
+DIR="$(readlink -f "$DIR/../..")"
+cd "$DIR"
+
+DGRAPH="$DIR/dgraph/dgraph"
+RATEL_RELEASE="https://github.com/dgraph-io/ratel/releases/download/21.03/dgraph-ratel-linux.tar.gz"
+RATEL="$DIR/ratel"
+[ -f "$DGRAPH" ] || make -C "$DIR" -j
+[ -f "$RATEL" ] || ( wget "$RATEL_RELEASE" && tar axvf dgraph-ratel-linux.tar.gz )
+PIDS=()
+
+unset http_proxy https_proxy
+cleanup() {
+  kill ${PIDS[@]} &>/dev/null
+  wait ${PIDS[@]}
+  # rm -rf "$DIR/data"
+}
+trap cleanup EXIT
+
+wait_for_ctrl_c() {
+  read -r -d '' _ </dev/tty
+}
+
+zero() {
+  local DATADIR="$DIR/data/zero"
+  "$DGRAPH" zero -v2 \
+    --log_dir=$DATADIR \
+    --wal=$DATADIR/w \
+    --telemetry="reports=false;sentry=false;" \
+    &
+    # &>/dev/null &
+  PIDS+=($!)
+  sleep 5
+}
+
+alpha() {
+  local ID=$1
+  local DATADIR="$DIR/data/alpha$ID"
+  "$DGRAPH" alpha -v2 \
+    --security "whitelist=0.0.0.0/0;" \
+    --log_dir=$DATADIR \
+    --my=0.0.0.0:$((7080+ID)) \
+    --zero=0.0.0.0:5080 \
+    --port_offset=$ID \
+    --tmp=$DATADIR/t \
+    --wal=$DATADIR/w \
+    --postings=$DATADIR/p \
+    --telemetry="reports=false;sentry=false;" \
+    &
+    # &>/dev/null &
+  PIDS+=($!)
+}
+
+ratel() {
+  "$RATEL" &
+  PIDS+=($!)
+}
+
+ratel
+zero
+alpha 0
+alpha 1
+alpha 2
+
+wait_for_ctrl_c
+
